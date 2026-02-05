36670a8be9659f29293437a9b078655caeffe38e
fotohabilaj <fotohabilaj@gmail.com>
2026-02-05 11:45:33 +0100

    correzione

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,7 +4,7 @@ const word_one = prompt('scrivi una parola');
 const word_two = prompt('scrivi una parola');
 
 if (word_one.length >= word_two.length) {
-    console.log(word_one, ',',word_two);
-} else {
     console.log(word_two, ',',word_one);
+} else {
+    console.log(word_one, ',',word_two);
 }
