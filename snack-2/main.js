bd1e471053b730dc241c8609f7eabfa8d118fc5c
fotohabilaj <fotohabilaj@gmail.com>
2026-02-05 11:24:35 +0100

    snack 2 complete

---
+++ snack-2/main.js
@@ -0,0 +1,10 @@
+console.log('hello');
+
+const word_one = (prompt('scrivi una parola'));
+const word_two = (prompt('scrivi una parola'));
+
+if (word_one.length >= word_two.length) {
+    console.log(word_one, ',',word_two);
+} else {
+    console.log(word_two, ',',word_one);
+}
