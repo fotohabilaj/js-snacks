54fac523e374ba2be759eda0da82aea707aed862
fotohabilaj <fotohabilaj@gmail.com>
2026-02-05 10:50:01 +0100

    corretto

--- snack-1/main.js
+++ snack-1/main.js
@@ -2,8 +2,9 @@ console.log('hello');
 
 const num_one = Number(prompt('digita un numero'));
 const num_two = Number(prompt('digita un numero'));
-if (num_one > num_two){
-    console.log(num_one,'maggiore');
+
+if (num_one > num_two) {
+    console.log('il primo è maggiore');
 } else if (num_two > num_one) {
-    console.log(num_two,'maggiore');
-} console.log('sono uguali');
+    console.log('il secondo è maggiore');
+} else {console.log('sono uguali')};
