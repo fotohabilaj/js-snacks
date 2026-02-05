137c5cb0046b28c17a5495c79723f6d25391545c
fotohabilaj <fotohabilaj@gmail.com>
2026-02-05 10:39:00 +0100

    snack 1 complete

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,4 +1,9 @@
 console.log('hello');
 
-const num_one= Number(prompt('digita un numero'));
-const num_two= Number(prompt('digita un numero'));
+const num_one = Number(prompt('digita un numero'));
+const num_two = Number(prompt('digita un numero'));
+if (num_one > num_two){
+    console.log(num_one,'maggiore');
+} else if (num_two > num_one) {
+    console.log(num_two,'maggiore');
+} console.log('sono uguali');
