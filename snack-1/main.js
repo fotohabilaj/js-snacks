09e0716bbd7985be4909c4931f5702a919da1529
fotohabilaj <fotohabilaj@gmail.com>
2026-02-05 10:57:09 +0100

    versione finale

--- snack-1/main.js
+++ snack-1/main.js
@@ -3,8 +3,8 @@ console.log('hello');
 const num_one = Number(prompt('digita un numero'));
 const num_two = Number(prompt('digita un numero'));
 
-if (num_one > num_two) {
+if (num_one >= num_two) {
     console.log(num_one);
-} else if (num_two > num_one) {
+} else(num_two > num_one) {
     console.log(num_two);
-} else {console.log(num_two)};
+}
